3198349cac410d68935159eb113724638fa140a1
shreyansjain01 <shreyansjain0503@gmail.com>
2024-07-04 23:37:46 +0530

    first commit

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,22 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { NavLink } from "react-router-dom"; // Use NavLink for active link styling
 import "./Navbar.css";
 import logo from "./images/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="logo">
-        <img src={logo} alt="Company Logo" />
+    <nav className="navbar" style={{ backgroundColor: '#ffffff' }}>
+      <div className="navbar-container">
+        <div className="logo">
+          <NavLink to="/">
+            <img src={logo} alt="Company Logo" />
+          </NavLink>
+        </div>
+        <div className="links">
+          <NavLink to="/" exact className="nav-button" activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="nav-button" activeClassName="active">
+            About
+          </NavLink>
+          <NavLink to="/services" className="nav-button" activeClassName="active">
+            Services
+          </NavLink>
+          <NavLink to="/contact" className="nav-button" activeClassName="active">
+            Contact
+          </NavLink>
+        </div>
       </div>
-      <div className="links">
-        {/* Use the Link component to navigate to the root URL */}
-        <Link to="/" className="nav-button">
-          Home
-        </Link>
-        <Link to="/" className="nav-button"></Link>
-      </div>
-    </div>
+    </nav>
   );
 };
 
--- my-app/src/components/bigDiv.js
+++ my-app/src/components/bigDiv.js
@@ -1,15 +1,14 @@
 import React from "react";
 import elderlyCare from "./images/elderly-care.jpg";
 import "./bigDiv.css";
-import description from "./images/desc.png";
 
-const bigDiv = () => {
+const BigDiv = () => {
   return (
     <div className="bigImg">
-      <img src={elderlyCare} alt="elderly-care" className="bigImage1"></img>
-      <img src={description} alt="description" className="bigImage2"></img>
+      <img src={elderlyCare} alt="elderly-care" className="bigImage1" />
+      <h1>Welcome to Viridian, A holistic platform made for your healthcare and wealthcare of the elderly</h1>
     </div>
   );
 };
 
-export default bigDiv;
+export default BigDiv;
